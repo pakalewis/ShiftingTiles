b81c38dd08d874cecbbf7c68842770f77fb0fb44
Parker <pakalewis@gmail.com>
2015-01-12 21:40:25 -0800

    Fix some autolayout issues

--- ShiftingTiles/MainScreen.swift
+++ ShiftingTiles/MainScreen.swift
@@ -20,15 +20,11 @@ class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
 
     
     // CONSTRAINTS
-    @IBOutlet weak var scrollTopConstraint: NSLayoutConstraint!
-    @IBOutlet weak var scrollViewLeftConstraint: NSLayoutConstraint!
     
     // VIEWS
     @IBOutlet weak var imageCollection: UICollectionView!
     @IBOutlet weak var imageCycler: UIImageView!
     @IBOutlet weak var tilesPerRowLabel: UILabel!
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var infoText: UITextView!
     
     @IBOutlet weak var letsPlayButton: UIButton!
     
@@ -70,46 +66,32 @@ class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
         
         
         
-        let scrollTapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("scrollTapped:"))
-        scrollTapGestureRecognizer.numberOfTapsRequired = 1
-        scrollTapGestureRecognizer.enabled = true
-        self.infoText.addGestureRecognizer(scrollTapGestureRecognizer)
-//        self.infoText.userInteractionEnabled = false
-        
-        self.infoText.text = "The objective of the game is to form the original image by shifting the tiles around until they are in the proper order. Tap one tile and then another to swap their positions. The black arrows on the top and left of the tiles allow entire rows and columns to swap postions.\n\nThe HINT button shows the first incorrect tile and the correct tile which it should be swapped with.\n\nThe SOLVE button will auto-solve the puzzle by swapping tiles until complete.\n\nUse the Show Original button to remind yourself what the original image looks like.\n\nImages were culled from unsplash.com and from Dale Arveson: phalconphotography.smugmug.com\n\nFeedback, questions, comments are welcome: pakalewis@gmail.com\n\nThe source code can be viewed here: github.com/pakalewis/shiftingtiles\n\ntest\n\ntest\n\ntest\n\ntest\n\ntest\n\ntest\n\ntest\n\ntest\n\ntest"
-
+//        let scrollTapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("scrollTapped:"))
+//        scrollTapGestureRecognizer.numberOfTapsRequired = 1
+//        scrollTapGestureRecognizer.enabled = true
+//        self.infoText.addGestureRecognizer(scrollTapGestureRecognizer)
+////        self.infoText.userInteractionEnabled = false
+//        
+//        self.infoText.text = "The objective of the game is to form the original image by shifting the tiles around until they are in the proper order. Tap one tile and then another to swap their positions. The black arrows on the top and left of the tiles allow entire rows and columns to swap postions.\n\nThe HINT button shows the first incorrect tile and the correct tile which it should be swapped with.\n\nThe SOLVE button will auto-solve the puzzle by swapping tiles until complete.\n\nUse the Show Original button to remind yourself what the original image looks like.\n\nImages were culled from unsplash.com and from Dale Arveson: phalconphotography.smugmug.com\n\nFeedback, questions, comments are welcome: pakalewis@gmail.com\n\nThe source code can be viewed here: github.com/pakalewis/shiftingtiles\n\ntest\n\ntest\n\ntest\n\ntest\n\ntest\n\ntest\n\ntest\n\ntest\n\ntest"
+//
     }
 
-    override func viewDidAppear(animated: Bool) {
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         self.drawGrid?.removeFromSuperview()
-        self.drawGrid = DrawGrid(frame: self.imageCycler.frame)
-        self.drawGrid?.numRows = self.tilesPerRow
-        self.drawGrid?.backgroundColor = UIColor.clearColor()
-        self.drawGrid?.frame = self.imageCycler.frame
-        self.view.addSubview(self.drawGrid!)
-        
-
     }
     
     
     // Segue to game screen
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
-        
-        if segue.identifier == "showStats" {
-            
-        }
-        
         if segue.identifier == "playGame" {
             var gameScreen = segue.destinationViewController as GameScreen
-            
             gameScreen.imageToSolve = self.imageToSolve
             gameScreen.tilesPerRow = self.tilesPerRow            
         }
-        
     }
     
 
-    
     
     //MARK: COLLECTION VIEW
     // Number of cells = number of images
@@ -117,86 +99,86 @@ class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
         return self.imageArray.count
     }
     
+    
     // Cells will be square sized
     func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, sizeForItemAtIndexPath indexPath: NSIndexPath!) -> CGSize {
         return CGSize(width: self.imageCollection.frame.height * 0.9, height: self.imageCollection.frame.height * 0.9)
-
     }
 
+    
     // Create cell from nib and load the appropriate image
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = self.imageCollection.dequeueReusableCellWithReuseIdentifier("CELL", forIndexPath: indexPath) as CollectionViewImageCell
         cell.imageView.image = self.smallImageArray[indexPath.row]
         return cell
-        
     }
+    
     
     // Selecting a cell loads the image to the main image view
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         self.imageToSolve = imageArray[indexPath.row]
         self.imageCycler.image = imageArray[indexPath.row]
-
     }
-    
     
     
     
     // MARK: Button actions
     @IBAction func infoButtonPressed(sender: AnyObject) {
 
-        self.view.bringSubviewToFront(self.scrollView)
-        self.scrollViewLeftConstraint.constant = 0
-        
-        //TODO: Why does the scroll view start somewhere in the middle of the text when it first appears?
-        var scrollRect = self.scrollView.frame
-//        self.scrollView.scrollRectToVisible(CGRectMake(0, 0, scrollRect.width, scrollRect.height), animated: false)
-        
-        self.scrollView.setContentOffset(CGPointMake(0, self.scrollView.contentInset.top), animated: false)
-        
-        
-        UIView.animateWithDuration(0.3, animations: { () -> Void in
-            self.view.layoutIfNeeded()
-        })
+//        self.view.bringSubviewToFront(self.scrollView)
+//        self.scrollViewLeftConstraint.constant = 0
+//        
+//        //TODO: Why does the scroll view start somewhere in the middle of the text when it first appears?
+//        var scrollRect = self.scrollView.frame
+////        self.scrollView.scrollRectToVisible(CGRectMake(0, 0, scrollRect.width, scrollRect.height), animated: false)
+//        
+//        self.scrollView.setContentOffset(CGPointMake(0, self.scrollView.contentInset.top), animated: false)
+//        
+//        
+//        UIView.animateWithDuration(0.3, animations: { () -> Void in
+//            self.view.layoutIfNeeded()
+//        })
         
     }
 
     func scrollTapped(recognizer: UITapGestureRecognizer) {
         
-        self.scrollViewLeftConstraint.constant = 1000
-        UIView.animateWithDuration(0.3, animations: { () -> Void in
-            self.view.layoutIfNeeded()
-        })
+//        self.scrollViewLeftConstraint.constant = 1000
+//        UIView.animateWithDuration(0.3, animations: { () -> Void in
+//            self.view.layoutIfNeeded()
+//        })
         
     }
-    
-    
 
     
     @IBAction func cameraButtonPressed(sender: AnyObject) {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+        var pickPhotoMenu = UIAlertController(title: "Choose a photo from your library or take one with the camera", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+        let libraryAction = UIAlertAction(title: "Library", style: UIAlertActionStyle.Default) { (handler) -> Void in
             let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
             imagePicker.allowsEditing = true
-            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
             self.presentViewController(imagePicker, animated: true, completion: nil)
-        } else {
-            
-            var noCameraAlert = UIAlertController(title: "", message: "No camera is available on this device", preferredStyle: UIAlertControllerStyle.Alert)
-            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil)
-            noCameraAlert.addAction(okAction)
-            self.presentViewController(noCameraAlert, animated: true, completion: nil)
         }
-    }
-    
-    
-    
-    @IBAction func galleryButtonPressed(sender: AnyObject) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.allowsEditing = true
-        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-        self.presentViewController(imagePicker, animated: true, completion: nil)
-    
+        let cameraAction = UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default) { (handler) -> Void in
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.allowsEditing = true
+                imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+                self.presentViewController(imagePicker, animated: true, completion: nil)
+            } else {
+                var noCameraAlert = UIAlertController(title: "", message: "No camera is available on this device", preferredStyle: UIAlertControllerStyle.Alert)
+                let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil)
+                noCameraAlert.addAction(okAction)
+                self.presentViewController(noCameraAlert, animated: true, completion: nil)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        pickPhotoMenu.addAction(libraryAction)
+        pickPhotoMenu.addAction(cameraAction)
+        pickPhotoMenu.addAction(cancelAction)
+        self.presentViewController(pickPhotoMenu, animated: true, completion: nil)
     }
     
     
@@ -213,7 +195,7 @@ class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
     
   
     
-    
+    // MARK: ImagePicker
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
         var imagePicked = info[UIImagePickerControllerEditedImage] as? UIImage
         
@@ -238,13 +220,13 @@ class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
     }
     
     
-    
-    
     func imagePickerControllerDidCancel(picker: UIImagePickerController!) {
         //this gets fired when the users cancel out of the process
         picker.dismissViewControllerAnimated(true, completion: nil)
     }
 
+
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -257,7 +239,7 @@ class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
     
     // MARK: Custom Stepper
     
-    @IBAction func upButtonPressed(sender: AnyObject) {
+    @IBAction func rightButtonPressed(sender: AnyObject) {
 
         self.tilesPerRow++
         if self.tilesPerRow > 10 {
@@ -274,7 +256,7 @@ class MainScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDa
         self.view.addSubview(self.drawGrid!)
    }
 
-    @IBAction func downButtonPressed(sender: AnyObject) {
+    @IBAction func leftButtonPressed(sender: AnyObject) {
         
         self.tilesPerRow--
         if self.tilesPerRow < 2 {
